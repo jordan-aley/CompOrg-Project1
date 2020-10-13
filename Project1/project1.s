5dbe096e0875a913699bde85376b0342c1e8b477
Jordan Aley <al3y05@gmail.com>
2020-10-13 10:47:26 -0400

    added low and hi input

--- Project1/project1.s
+++ Project1/project1.s
@@ -1,10 +1,18 @@
-.data #let processor know we will be submitting data to program now
+#let processor know we will be submitting data to program now
+.data
 
 insert_into:
-	.word 4 #make a 4 byte (32 bit) space in memory for a word with address insert_into
+    #make a 4 byte (32 bit) space in memory for a word with address insert_into
+	.word 4
 
 Ask_Input:
-	.asciiz "\Enter a string that is exactly 10 characters\n" #in unused memory store this string with address Ask_Input
+    #in unused memory store this string with address Ask_Input
+	.asciiz "\Enter a string that is exactly 10 characters\n"
 
+Low_Input:
+    
+    .asciiz "\Entered a string less than 10 characters, the program closed\n"
 
+High_Input:
+    .asciiz "\Entered a string greater than 10 characters, the program closed\n"
 
