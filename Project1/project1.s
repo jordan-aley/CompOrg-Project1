0c3081a1caf4400ed5ff14622174fc6a076b0431
Jordan Aley <al3y05@gmail.com>
2020-10-15 10:26:09 -0400

    load input and check if input is 10 characters

--- Project1/project1.s
+++ Project1/project1.s
@@ -11,3 +11,13 @@
     la $a0, input
     li $a1, 11
     syscall
+
+    #loads the input from the user
+    load_input:
+    move $t3, $a0
+    lb $s2, ($t3)
+
+    #checks if input is equal to 10 characters
+    check:
+    beq $t0, 10, finish
+
