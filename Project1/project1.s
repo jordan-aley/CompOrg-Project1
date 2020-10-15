e235e8d00f9bf0a0ad12cd147249c403f14bc911
Jordan Aley <al3y05@gmail.com>
2020-10-15 09:34:05 -0400

    restarted

--- Project1/project1.s
+++ Project1/project1.s
@@ -1,18 +1,9 @@
-#let processor know we will be submitting data to program now
 .data
-
-insert_into:
-    #make a 4 byte (32 bit) space in memory for a word with address insert_into
-	.word 4
-
-Ask_Input:
-    #in unused memory store this string with address Ask_Input
-	.asciiz "\Enter a string that is exactly 10 characters\n"
-
-Low_Input:
+    #user input
+    input: .space 11
+    #makes new line
+    newLine: .asciiz "\n"
     
-    .asciiz "\Entered a string less than 10 characters, the program closed\n"
+.text
 
-High_Input:
-    .asciiz "\Entered a string greater than 10 characters, the program closed\n"
 
